a7165751e6175bceb12a5457bce5fd5b5b0a6555
Jerkbert <raballantyne@gmail.com>
2020-02-20 15:53:26 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './dist/css/style.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>smoke</h1>
+    </div>
+  );
+}
+
+export default App;
